5af022af6c97fd09cf75d532e5a29c35d9c5f6b2
Cristian Santiz <crisantizan@gmail.com>
2020-10-12 16:49:32 -0500

    feat: create components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VDropdown v-slot="{ show, toggleFn }">
+      <button @click="toggleFn">Toggle</button>
+      <VDropdownMenu :show="show"></VDropdownMenu>
+    </VDropdown>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VDropdown from './components/VDropdown.vue';
+import VDropdownMenu from './components/VDropdownMenu.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    VDropdown,
+    VDropdownMenu,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/VDropdown.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="VDropdown">
+		<slot v-bind:show="show" v-bind:toggleFn="toggleFn"></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		show: false,
+	}),
+
+	methods: {
+		toggleFn() {
+			this.show = !this.show;
+		},
+	},
+};
+</script>
+
+<style scoped>
+.VDropdown {
+	position: relative;
+}
+</style>
